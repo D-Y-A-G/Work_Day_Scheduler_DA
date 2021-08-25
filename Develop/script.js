128d1691933becbdef14e6103247a32686959dbb
Denis Arce <dnsarce21@gmail.com>
2021-08-25 14:04:01 -0500

    working on .js file

--- Develop/script.js
+++ Develop/script.js
@@ -1,4 +1,16 @@
+// setup time to display below title
 var now = moment().format("dddd, MMMM Do YYYY");
 document.getElementById("currentDay").textContent = now;
 
-// you can add and take away classes look up toggle
+//  note you can add and take away classes look up toggle
+
+// write function that defines current hour and is higlighted red
+
+
+// write function that defines time that has passed and is highlighted gray makes element unavailable
+
+
+//write function to save input written on textarea
+
+
+

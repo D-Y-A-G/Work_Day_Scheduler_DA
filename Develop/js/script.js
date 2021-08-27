d12497124b32c272e71ac0b941e050d5323bfe74
Denis Arce <dnsarce21@gmail.com>
2021-08-26 21:57:20 -0500

    fixing link to css and js

--- Develop/js/script.js
+++ Develop/js/script.js
@@ -1,16 +1,24 @@
 // setup time to display below title
 var now = moment().format("dddd, MMMM Do YYYY");
 document.getElementById("currentDay").textContent = now;
+////////////////////TIME BLOCKS //////////////////////////
+
+function currentTime() {
+  var currentHour = moment().hour();
+
+  if (currentHour === now) {
+    currentHour.classList.toggle("present");
+  }
+}
 
 //  note you can add and take away classes look up toggle
 
-// write function that defines current hour and is higlighted red
+// need to write function that
 
+// write function that presents current hour and highlights hour block red
 
-// write function that defines time that has passed and is highlighted gray makes element unavailable
+// write function that states time that has passed and is highlighted gray
 
+//write function that states time is in future highlighte green
 
 //write function to save input written on textarea
-
-
-

909fa3e6a75bba3b606e73441a3cc059d4dcc2bd
Denis Arce <dnsarce21@gmail.com>
2021-08-28 23:14:05 -0500

    updated code

--- Develop/js/script.js
+++ Develop/js/script.js
@@ -1,24 +1,52 @@
 // setup time to display below title
 var now = moment().format("dddd, MMMM Do YYYY");
-document.getElementById("currentDay").textContent = now;
-////////////////////TIME BLOCKS //////////////////////////
+document.getElementById("currentDay").textContent = now; // try query selector
+////////////////////TIME BLOCKS ////////////////////////// defining time block variables
 
-function currentTime() {
-  var currentHour = moment().hour();
+// write with current hour past and future and highlights based on color red now green future gray past
+var hours = {
+  8: $("hourBlock"),
+  9: $("hourBlock"),
+  10: $("hourBlock"),
+  11: $("hourBlock"),
+  12: $("hourBlock"),
+  1: $("hourBlock"),
+  2: $("hourBlock"),
+  3: $("hourBlock"),
+  4: $("hourBlock"),
+};
+// write function that states time that has passed and references classes in css 
 
-  if (currentHour === now) {
-    currentHour.classList.toggle("present");
-  }
-}
+var hourly = moment().format("h:m");
+console.log(hourly);
 
-//  note you can add and take away classes look up toggle
+$(".hourBlock").each(function(){
+  var currentHour = $(this).data("time");
 
-// need to write function that
+})
 
-// write function that presents current hour and highlights hour block red
+$(document).ready(function () {
+  console.log(hourly);
+});
+// need to loop through each block to check time and apply correct background
+$(".time-block").each(function () {});
+///---local storage--- need to save user input when save button is clicked and then reload that info from local storage when page is reloaded.///
 
-// write function that states time that has passed and is highlighted gray
+$(".saveBtn").on("click", function (event) {
+  event.preventDefault();
+  var timeBlock = $(this).parent().prev().children();
+  //localStorage.setItem("content", timeBlock);
+  //localStorage.getItem("content");
 
-//write function that states time is in future highlighte green
+  console.log(timeBlock[0].val);
+});
 
-//write function to save input written on textarea
+//// local storage set and get item
+//localStorage.setItem("eightAm", eight);
+// localStorage.getItem("eightAm");
+
+// localStorage.setItem("nineAm", nine);
+// localStorage.getItem("nineAm");
+
+// localStorage.setItem("tenAm", ten);
+// localStorage.getItem("")
